69b69ae0e55b93d9f103c17c196f9c9ebbc7d974
gunfury <unnivishnu18@gmail.com>
2024-05-09 13:00:44 +0530

    secondcommit

---
+++ models/couponModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const couponSchema = new Schema({
+   
+    couponCode: {
+        type: String,
+    },
+    discount: {
+        type: Number,
+    },
+    expiryDate: {
+        type: Date,
+    },
+    minimumPurchaseAmount: {
+        type: Number,
+    },
+    purchaseAmount: {
+        type: Number,
+    }
+
+});
+
+const Coupon = mongoose.model('Coupon', couponSchema);
+module.exports=Coupon;
